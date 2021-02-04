c88a3aaa78f40a505bd257b51fcce01bf917004b
lamyin_ngai <wlxjust@gmail.com>
2021-02-04 13:04:00 +0800

    add UGLIFY_BUG_REPORT dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
+const UGLIFY_BUG_REPORT_ENABLE = !!process.env.UGLIFY_BUG_REPORT;
+
 module.exports = {
   //   mode: 'development',
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'uglify.bundle.js',
+    filename: UGLIFY_BUG_REPORT_ENABLE
+      ? 'uglify-bug-report.bundle.js'
+      : 'uglify.bundle.js',
   },
   module: {
     rules: [
@@ -23,6 +27,12 @@ module.exports = {
     ],
   },
   optimization: {
-    minimizer: [new UglifyJSPlugin()],
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          compress: true,
+        },
+      }),
+    ],
   },
 };
